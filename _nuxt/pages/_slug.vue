99507a3db9ccd1f356119950045ac9a62f17f718
Vinícius <viniciusarre@gmail.com>
2020-08-28 18:59:00 -0300

    lint fixes and content boilerplate

---
+++ _nuxt/pages/_slug.vue
@@ -0,0 +1,12 @@
+<template>
+  <nuxt-content />
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const doc = await $content(params.slug || 'index').fetch()
+    return { doc }
+  }
+}
+</script>
