ff24e7154430424abc200a0916ad9f2303db4b35
Vinícius <viniciusarre@gmail.com>
2020-09-07 21:11:45 -0300

    Make navbar and menu more responsive

--- components/SideContainer.vue
+++ components/SideContainer.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="mt-10">
     <v-row class="justify-center">
-      <v-img src="/logo.png" height="auto" max-width="200" position="center center" />
+      <v-img src="/logo.png" class="hidden-sm-and-down" height="auto" max-width="200" position="center center" />
+      <v-img class="hidden-md-and-up" src="/logo.png" height="auto" max-width="150" position="center center" />
     </v-row>
-    <div class="py-10">
-      <h2 class="text-center">
-        Vinícius Arré
+    <div class="py-xl-10 py-sm-2">
+      <h2 class="text-center hidden-sm-and-down">
+        Vinícius Arré's Web Development blog
+      </h2>
+      <h2 class="text-center hidden-md-and-up">
+        Vinícius' Blog
       </h2>
     </div>
   </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,45 @@
 <template>
   <v-app>
-    <v-navigation-drawer app class="aside-background">
+    <v-app-bar class="hidden-md-and-up" dark>
+      <v-app-bar-nav-icon @click="mobileNavbar = !mobileNavbar" />
+    </v-app-bar>
+    <!-- default navbar -->
+    <v-navigation-drawer app class="aside-background hidden-sm-and-down">
       <side-container />
     </v-navigation-drawer>
-    <v-main class="content" dark>
+    <!-- mobile navbar -->
+
+    <v-navigation-drawer v-model="mobileNavbar" absolute class="mobile-background hidden-md-and-up">
+      <div class="text-right mr-3 mt-5">
+        <v-icon @click="mobileNavbar = !mobileNavbar">
+          mdi-close
+        </v-icon>
+        <side-container :mobile="true" />
+      </div>
+    </v-navigation-drawer>
+    <v-main class="content">
       <nuxt />
     </v-main>
   </v-app>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      mobileNavbar: false
+    }
+  }
+}
+</script>
+
 <style scoped>
     .aside-background {
+      background-color: lightblue;
+      color: black;
+    }
+    .mobile-background {
+      min-width: 100vw;
       background-color: lightblue;
       color: black;
     }
